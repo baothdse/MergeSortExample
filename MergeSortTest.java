9ec37719d8f02c8a74f0d60808665043401c1083
baothdse <baothdse@gmail.com>
2016-11-17 14:06:09 +0700

    Update MergeSortTest.java
    Done Merger Sort with an ArrayList of Number

--- MergeSortTest.java
+++ MergeSortTest.java
@@ -7,12 +7,16 @@ public class MergeSortTest {
 		MergeSortImplement test = new MergeSortImplement();
         ArrayList<Integer> listOfNumber = new ArrayList<Integer>();
         listOfNumber.add(5);
-        listOfNumber.add(8);
+        listOfNumber.add(10);
         listOfNumber.add(1);
         listOfNumber.add(4);
         listOfNumber.add(7);
         listOfNumber.add(9);
         listOfNumber.add(3);
+        listOfNumber.add(20);
+        listOfNumber.add(10);
+        listOfNumber.add(13);
         test.mergeSort(listOfNumber);
+        System.out.println(listOfNumber);
     }
 }
