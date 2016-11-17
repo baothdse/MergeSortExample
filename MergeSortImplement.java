16403bfb389037426ccf129591982493a9b36799
baothdse <baothdse@gmail.com>
2016-11-17 14:05:14 +0700

    Update MergeSortImplement.java
    Done Merge Sort in an ArrayList of number

--- MergeSortImplement.java
+++ MergeSortImplement.java
@@ -1,18 +1,16 @@
 package merge;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class MergeSortImplement { 
-	ArrayList<Integer> listOfNumber = new ArrayList<Integer> ();
-	public void merge(ArrayList<Integer> numberList, ArrayList<Integer> leftNumberList, ArrayList<Integer> rightNumberList) {
+public class MergeSortImplement {
+	public void merge(ArrayList<Integer> numberList, ArrayList<Integer> leftNumberList,
+			ArrayList<Integer> rightNumberList) {
 		int index = 0;
 		int leftIndex = 0;
 		int rightIndex = 0;
-		
-		while (leftIndex <= leftNumberList.size() && rightIndex <= rightNumberList.size()) {
-			if (leftNumberList.indexOf(leftIndex) < rightNumberList.indexOf(rightIndex)) {
+
+		while (leftIndex < leftNumberList.size() && rightIndex < rightNumberList.size()) {
+			if (leftNumberList.get(leftIndex) < rightNumberList.get(rightIndex)) {
 				numberList.set(index, leftNumberList.get(leftIndex));
 				index++;
 				leftIndex++;
@@ -22,40 +20,42 @@ public class MergeSortImplement {
 				rightIndex++;
 			}
 		}
-		
-		while (leftIndex <= leftNumberList.size()) {
-			numberList.set(index, leftNumberList.get(leftIndex));
-			index++;
-			leftIndex++;
+
+		if (rightIndex >= rightNumberList.size()) {
+			while (leftIndex < leftNumberList.size()) {
+				numberList.set(index, leftNumberList.get(leftIndex));
+				index++;
+				leftIndex++;
+			}
 		}
-		
-		while (rightIndex <= rightNumberList.size()) {
-			numberList.set(index, rightNumberList.get(rightIndex));
-			index++;
-			rightIndex++; 
+
+		if (leftIndex >= leftNumberList.size()) {
+			while (rightIndex < rightNumberList.size()) {
+				numberList.set(index, rightNumberList.get(rightIndex));
+				index++;
+				rightIndex++;
+			}
 		}
+
 	}
-	
+
 	public void mergeSort(ArrayList<Integer> numberList) {
 		if (numberList.size() == 1) {
 			return;
 		}
-		int leftIndex = 0;
-		int rightIndex = 0; 
-		int mid = numberList.size()/2;
+		int mid = numberList.size() / 2;
 		ArrayList<Integer> leftNumberList = new ArrayList<Integer>();
 		ArrayList<Integer> rightNumberList = new ArrayList<Integer>();
 		for (int i = 0; i < mid; i++) {
 			leftNumberList.add(numberList.get(i));
 		}
-		
+
 		for (int i = mid; i <= numberList.size() - 1; i++) {
 			rightNumberList.add(numberList.get(i));
 		}
-		
 		mergeSort(leftNumberList);
 		mergeSort(rightNumberList);
-		
-		merge(numberList,leftNumberList, rightNumberList);
+
+		merge(numberList, leftNumberList, rightNumberList);
 	}
 }
